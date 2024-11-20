06a68b62a6ad41767b7f8f74b113185888710551
unknown <Ashik-Rahman100>
2024-11-20 22:36:31 +0600

    [feature] create and get academic semester

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -15,7 +15,7 @@ export default function MainLayout() {
     toast.success("Successfully logout", { duration: 2000 });
   };
   return (
-    <Layout style={{ height: "100vh" }}>
+    <Layout style={{ height: "100%" }}>
       <Sidebar />
       <Layout>
         <Header style={{ padding: 0 }}>
