94149ee36599b21b63c79308b829909467ed6b5a
unknown <Ashik-Rahman100>
2024-11-29 16:29:44 +0600

    [feature] add create student

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -13,6 +13,19 @@ export const monthsName = [
   "December",
 ];
 
+export const genders = ["Male", "Female", "Other"];
+
+export const bloodGroups = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"];
+
+export const genderOptions = genders.map((item) => ({
+  value: item.toLowerCase(),
+  label: item,
+}));
+export const bloodGroupsOptions = bloodGroups.map((item) => ({
+  value: item,
+  label: item,
+}));
+
 export const monthOptions = monthsName.map((item) => ({
   value: item,
   label: item,
